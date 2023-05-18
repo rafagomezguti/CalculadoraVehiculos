ac0cb72869ab5ca8b9c412e9084e43ef2691a9db
rafagomezguti <126033795+rafagomezguti@users.noreply.github.com>
2023-05-18 18:09:45 -0500

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,24 @@
+function calcular(){
+
+
+    var avaluo = parseInt(document.getElementById("avaluo").value);
+    var impuesto = parseInt(document.getElementById("impuesto").value);
+    var placa = document.getElementById("placa").value;
+    var impuesto = parseInt(document.getElementById("impuesto").value);
+    var condicionCheckbox = document.getElementById("condicionCheckbox");
+
+    //variables
+    var traspaso = 145000;
+    var ministerio = 29700;
+    var runt = 4900;
+    var avaluo = avaluo * 0.01;
+    var prenda = 127000;
+   
+    //Operacion
+    if (condicionCheckbox.checked) {
+      var resultado = avaluo + impuesto + ministerio + runt + traspaso + prenda;
+    }else{
+      var resultado = avaluo + impuesto + ministerio + runt + traspaso;
+    }
+    document.getElementById("resultado").innerHTML = "Para el vehiculo de placa "+ placa +" el valor del traspaso es: "+ resultado;
+}
