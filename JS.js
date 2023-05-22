c76f3b5bbe578f4e8f06a6590afeed5866ca50ae
rafagomezguti <126033795+rafagomezguti@users.noreply.github.com>
2023-05-21 19:28:00 -0500

    Update JS.js

--- JS.js
+++ JS.js
@@ -25,3 +25,11 @@ function letraMayuscula() {
       var input = document.getElementById('placa');
       input.value = input.value.toUpperCase();
     }
+
+
+function formatNumber() {
+      var input = document.getElementById('avaluo');
+      var value = input.value.replace(/\D/g, ''); // Elimina todos los caracteres que no sean dígitos
+      var formattedValue = Number(value).toLocaleString('es-ES'); // Formatea el número con separación de miles utilizando el local 'es-ES'
+      input.value = formattedValue;
+    }
